d84def5004e1569ab7da04e2bb50270658ce7d76
RobertsOta <roberts.otankis@gmail.com>
2022-05-06 20:17:23 +0300

    Datubāzes savienojums izveidots

--- src/main/java/com/example/autenticatask/DatabaseConnection.java
+++ src/main/java/com/example/autenticatask/DatabaseConnection.java
@@ -7,17 +7,28 @@ import java.sql.Statement;
 import java.sql.*;
 
 public class DatabaseConnection {
-    final String DB = "jdbc:mysql://autdatabase.cjbekmfmnqfk.eu-north-1.rds.amazonaws.com/";
-    //"jdbc:mysql://rcs-database.cgox3orafoc1.us-east-1.rds.amazonaws.com:3306/RcsHomework"
-    final String USERNAME = "admin";
-    final String PASSWORD = "1!priedes135";
+
+    //Establish connection with database
+    private static Connection getConnection() throws SQLException {
+
+        final String DB = "jdbc:mysql://autdatabase.cjbekmfmnqfk.eu-north-1.rds.amazonaws.com:3306/hardwareRequests";
+        final String USERNAME = "admin";
+        final String PASSWORD = "1!priedes135";
+
+        Connection conn = DriverManager.getConnection(DB, USERNAME, PASSWORD);
+
+        return conn;
+    }
+
+
+
 
     public void addRequest() {
         try {
-            Connection conn = DriverManager.getConnection(DB, USERNAME, PASSWORD);
+            Connection conn = DatabaseConnection.getConnection();
 
             Statement stmt = conn.createStatement();
-            String sqlQuery = "INSERT INTO users (hardware, parameters, comments)" + "VALUES (?, ?, ?)";
+            String sqlQuery = "INSERT INTO requests (hardware, parameters, comment)" + "VALUES (?, ?, ?)";
             PreparedStatement prepStmt = conn.prepareStatement(sqlQuery);
 
             prepStmt.setString(1, "Monitors");
@@ -33,46 +44,4 @@ public class DatabaseConnection {
             e.printStackTrace();
         }
     }
-
-
-//     try{
-//        Connection conn = DriverManager.getConnection(DB, USERNAME, PASSWORD);
-//
-//        Statement stmt = conn.createStatement();
-//        String sql = "INSERT INTO users (name, email, phone, city, address, password) " + "VALUES (?, ?, ?, ?, ?, ?)";
-//
-//        PreparedStatement preparedStatement = conn.prepareStatement(sql);
-//
-//        preparedStatement.setString(1, name);
-//        preparedStatement.setString(2, email);
-//        preparedStatement.setString(3, phone);
-//        preparedStatement.setString(4, city);
-//        preparedStatement.setString(5, address);
-//        preparedStatement.setString(6, password);
-//
-//
-////Pievienot ierakstu
-//        int addedRows = preparedStatement.executeUpdate();
-//        if (addedRows > 0) {
-//            user = new User();
-//            user.name = name;
-//            user.email = email;
-//            user.phone = phone;
-//            user.city = city;
-//            user.address = address;
-//            user.password = password;
-//        }
-//
-//        stmt.close();
-//        conn.close();
-//
-//
-//    } catch (SQLException e) {
-//        e.printStackTrace();
-//    }
-//
-//
-//        return user;
-//}
-
 }
